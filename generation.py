e1d50ad4561e88aae417e5bfbb7a01fcdc378d7d
thomasbarrepitous <barrepitousthomas@gmail.com>
2019-07-02 23:54:50 +0200

    Structure fix, début GA, graphe complet

---
+++ generation.py
@@ -0,0 +1,180 @@
+from random import randint
+
+import networkx as nx
+import matplotlib.pyplot as plt
+import json
+
+
+class VRPGenerator:
+    """Classe générant un graphe connexe et utilisable pour le problème VRP.
+    Caractérisé par :
+    - son réalisme (0 ou 1)
+    - un fichier json
+    """
+
+    # Liste qui va stocker les données du fichier JSON
+    data = []
+    # Notre graphe
+    G = 0
+    # Realisme activé ou désactivé (1 ou 0), définit si les villes sont placées en fonction de leur lat/lng ou
+    # aléatoirement
+    realisme = 0
+    # La variable contenant le path du fichier JSON
+    json_file = ''
+    # La limite d'arrêtes par sommets
+    LIMITE = 5
+    BORNEMIN = 0
+    BORNEMAX = 10
+    TAILLE_DATA = 0
+
+    def __init__(self, realisme, json_file, taille, bornemin, bornemax):
+        """Constructeur de notre classe"""
+        self.realisme = realisme
+        self.json_file = json_file
+        self.TAILLE_DATA = taille
+        self.BORNEMIN = bornemin
+        self.BORNEMAX = bornemax
+
+    def vrp_generate(self):
+        # JSON Config
+        # On charge le fichier json dans la variable data
+        with open(self.json_file) as f:
+            self.data = json.load(f)
+
+        self.G = nx.Graph()
+        self.reduire_echantillon(self.TAILLE_DATA)
+
+        if self.realisme == 1:
+            # On crée le triangle de départ
+            for i in range(0, 3):
+                self.G.add_node(self.data[i]["name"], pos=(self.lat(i), self.lng(i)))
+            self.G.add_edge(self.data[0]["name"], self.data[1]["name"], weight=randint(self.BORNEMIN, self.BORNEMAX))
+            self.G.add_edge(self.data[2]["name"], self.data[0]["name"], weight=randint(self.BORNEMIN, self.BORNEMAX))
+            self.G.add_edge(self.data[2]["name"], self.data[1]["name"], weight=randint(self.BORNEMIN, self.BORNEMAX))
+
+            self.G = self.vrp_realiste()
+
+            # On dessine notre graphe
+            nx.draw(self.G, nx.get_node_attributes(self.G, 'pos'), node_size=75, with_labels=True)
+            # pos = nx.get_node_attributes(vrp.G, 'pos')
+            # labels = nx.get_edge_attributes(vrp.G, 'weight')
+            # nx.draw_networkx_edge_labels(vrp.G, pos, edge_labels=labels)
+        elif self.realisme == 0:
+            for i in range(0, 3):
+                self.G.add_node(self.data[i]["name"])
+
+            self.G.add_edge(self.data[0]["name"], self.data[1]["name"], weight=randint(self.BORNEMIN, self.BORNEMAX))
+            self.G.add_edge(self.data[2]["name"], self.data[0]["name"], weight=randint(self.BORNEMIN, self.BORNEMAX))
+            self.G.add_edge(self.data[2]["name"], self.data[1]["name"], weight=randint(self.BORNEMIN, self.BORNEMAX))
+
+            self.G = self.vrp_standard()
+
+            # On dessine notre graphe
+            nx.draw(self.G, node_size=75, with_labels=True)
+            # pos = nx.get_node_attributes(vrp.G, 'pos')
+            # labels = nx.get_edge_attributes(vrp.G, 'weight')
+            # nx.draw_networkx_edge_labels(vrp.G, pos, edge_labels=labels)
+        elif self.realisme == 2:
+            for n in range(0, len(self.data)):
+                current_node = self.data[n]["name"]
+                self.G.add_node(current_node)
+
+            self.G = self.vrp_complet()
+
+            nx.draw(self.G, node_size=75, with_labels=True)
+            # pos = nx.get_node_attributes(vrp.G, 'pos')
+            # labels = nx.get_edge_attributes(vrp.G, 'weight')
+            # nx.draw_networkx_edge_labels(vrp.G, pos, edge_labels=labels)
+        return self.G;
+
+    def vrp_complet(self):
+        for i in range(0, len(self.data)):
+            current_node = self.data[i]["name"]
+            for j in range(0, len(self.data)):
+                node_cible = self.data[j]["name"]
+                self.G.add_edge(current_node, node_cible, weight=randint(self.BORNEMIN, self.BORNEMAX))
+        return self.G;
+
+    def vrp_standard(self):
+        # Pour tous les sommets à partir de 3 dans notre JSON
+        for j in range(3, len(self.data)):
+            # On génere un nombre d'arrête aléatoire entre 2 et n-1, n étant notre nombre de sommets
+            nbr_edge = self.limite_edge(randint(2, len(self.G.node) - 1), self.LIMITE)
+
+            current_node = self.data[j]["name"]
+
+            # On l'ajoute à notre graphe
+            self.G.add_node(current_node)
+
+            # On ajoute autant d'arrêtes que la variable nbr_edge
+            for n in range(1, nbr_edge + 1):
+                rand = randint(0, len(self.G.node) - 1)
+                cpt = 0
+
+                for node in self.G:
+                    if self.G.has_edge(current_node, node):
+                        cpt += 1
+                if cpt == len(self.G.node):
+                    break;
+
+                while self.G.has_edge(current_node, self.data[rand]["name"]) | rand == j:
+                    rand = randint(0, len(self.G.node) - 1)
+
+                node_cible = self.data[rand]["name"]
+                self.G.add_edge(current_node, node_cible, weight=randint(self.BORNEMIN, self.BORNEMAX))
+        return self.G;
+
+    def vrp_realiste(self):
+        # Pour tous les sommets à partir de 3 dans notre JSON
+        for j in range(3, len(self.data)):
+            # On génere un nombre d'arrête aléatoire entre 2 et n-1, n étant notre nombre de sommets
+            nbr_edge = self.limite_edge(randint(2, len(self.G.node) - 1), self.LIMITE)
+
+            current_node = self.data[j]["name"]
+
+            # On l'ajoute à notre graphe
+            self.G.add_node(current_node, pos=(self.lat(j), self.lng(j)))
+
+            # On ajoute autant d'arrêtes que la variable nbr_edge
+            for n in range(1, nbr_edge + 1):
+                rand = randint(0, len(self.G.node) - 1)
+                cpt = 0
+
+                for node in self.G:
+                    if self.G.has_edge(current_node, node):
+                        cpt += 1
+                if cpt == len(self.G.node):
+                    break;
+
+                while self.G.has_edge(current_node, self.data[rand]["name"]) | rand == j:
+                    rand = randint(0, len(self.G.node) - 1)
+
+                node_cible = self.data[rand]["name"]
+                self.G.add_edge(current_node, node_cible, weight=randint(self.BORNEMIN, self.BORNEMAX))
+        return self.G;
+
+    # Fonction qui va recupère la position dans la liste en latitude
+    def lat(self, i):
+        latitude = self.data[i]["lat"]
+        return float(latitude);
+
+    # Fonction qui va recupère la position dans la liste en longitude
+    def lng(self, i):
+        longitude = self.data[i]["lng"]
+        return float(longitude);
+
+    # Fonction qui limite le nombre d'arrêtes possible
+    def limite_edge(self, n, limite):
+        if n >= limite:
+            return n;
+        return randint(2, limite);
+
+    # Fonction utilisé pour réduire la taille de la liste
+    def reduire_echantillon(self, taille_data):
+        while len(self.data) > taille_data:
+            self.data.pop(1)
+        return self.data;
+
+    def show_graph(self):
+        plt.show()
+        return 0;
--- vrp_solving.py
+++ vrp_solving.py
@@ -1,22 +1,101 @@
-from random import random
-
-from main import VRPGenerator
+import random
 
 CHANCE_TO_MUTATE = 0.1
 GRADED_RETAIN_PERCENT = 0.2
 CHANCE_RETAIN_NONGRATED = 0.05
 
 POPULATION_COUNT = 100
+NB_VILLES = 5
 
-GENERATION_COUNT_MAX = 10000
+GENERATION_COUNT_MAX = 100
 
 GRADED_INDIVIDUAL_RETAIN_COUNT = int(POPULATION_COUNT * GRADED_RETAIN_PERCENT)
 
+MAXIMUM_FITNESS = 10 * NB_VILLES
 
-MAXIMUM_FITNESS = 10000
+SOMMET_DEPART = 0
 
 
+class Genetic_Algorithm:
+    """
+    """
 
-vrp = VRPGenerator(0, 'E:\Desktop\Dev\Projet DATA\CESI-Projet-DATA/villes.json')
+    BEST_IND = []
+    BEST_FITNESS = 0
 
-print(vrp)
+    fitness = []
+    population = []
+    vrp = 0
+    foo = [[0, 2, 1, 5, 4],
+           [1, 0, 9, 3, 7],
+           [5, 2, 0, 6, 9],
+           [2, 2, 1, 0, 1],
+           [3, 2, 3, 3, 0]]
+
+    def __init__(self, vrp):
+        """Constructeur de notre classe"""
+        self.vrp = vrp
+        self.NB_VILLES = vrp.TAILLE_DATA
+
+        self.population = self.gen_ppl_initiale()
+        self.population = self.tri_fitness(self.population)
+
+        self.muter([0, 1, 2, 3, 4, 5, 0])
+
+    def launch(self):
+
+        return self.BEST_IND, self.BEST_FITNESS;
+
+    # Etape 1 : On genere une population de base
+    def gen_ppl_initiale(self):
+        ppl = []
+        for i in range(0, self.vrp.TAILLE_DATA):
+            ppl.append(self.generation_ind(SOMMET_DEPART))
+        return ppl;
+
+    def generation_ind(self, depart):
+        chemin = []
+        for n in range(0, NB_VILLES):
+            chemin.append(n)
+        random.shuffle(chemin)
+        chemin.remove(0)
+        chemin.insert(0, depart)
+        chemin.insert(self.NB_VILLES, depart)
+        return chemin;
+
+    # Etape 2 : On évalue cette population
+
+    def eval_fitness(self, chemin):
+        score = 0
+        for index, value in enumerate(chemin):
+            if index != len(chemin) - 1:
+                value1 = chemin[index]
+                value2 = chemin[index + 1]
+                score += self.foo[value1][value2]
+        return score;
+
+    def tri_fitness(self, liste_entree):
+        foo = list(liste_entree)
+        N = len(foo)
+        for i in range(1, N):
+            cle = foo[i]
+            j = i - 1
+            while j >= 0 and self.eval_fitness(foo[j]) > self.eval_fitness(cle):
+                foo[j + 1] = foo[j]  # decalage
+                j = j - 1
+            foo[j + 1] = cle
+        return foo;
+
+    # Etape 3 : On fait muter la population
+    def mutation(self, ppl):
+        # On trie par rapport à la fitness
+
+        return ppl;
+
+    def muter(self, individu):
+        gene1 = random.randint(1, self.vrp.TAILLE_DATA)
+        gene2 = random.randint(1, self.vrp.TAILLE_DATA)
+        print(individu)
+        individu[gene1], individu[gene2] = individu[gene2], individu[gene1]
+        print(individu)
+        return individu;
